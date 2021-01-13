1b52ca2764121ca79ad0b6d6d9b0c5ab39fa3cb1
joseph walter <josephwalter@josephs-MacBook-Pro.local>
2021-01-12 18:24:20 -0700

    added .get and dependencies

--- app.js
+++ app.js
@@ -4,7 +4,12 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const port = process.env.PORT || 5555;
+app.use(bodyParser.json());
+app.use(cors());
 
--app.listen(port, () => {
-    console.log(`What port is active? it's port ${port}`);
-});
+app.get('/', (req,res,next) => {
+    
+})
+// app.listen(port, () => {
+//     console.log(`What port is active? it's port ${port}`);
+// });
