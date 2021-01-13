78d00c574ff8991802a93b31fcd50ad896933714
joseph walter <josephwalter@josephs-MacBook-Pro.local>
2021-01-12 17:44:35 -0700

    added nodemon

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const port = process.env.PORT || 5555;
+
+-app.listen(port, () => {
+    console.log(`What port is active? it's port ${port}`);
+});
